ce5ca307e23f448bbe197152494d4ebc3de9d68b
pascal masila <118198832+Mugiwara1uffy@users.noreply.github.com>
2026-03-18 09:31:19 +0300

    Save and deploy clean root config

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  // We are removing 'base' and 'build' objects. 
-  // Vite defaults are perfect for Vercel.
+  base: "/", 
 });
