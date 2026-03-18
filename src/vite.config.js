1140737fc5eafe6a9d379ded20825a0eb00cc15e
pascal masila <118198832+Mugiwara1uffy@users.noreply.github.com>
2026-03-18 09:54:33 +0300

    Final exorcism of the austine path

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/"
   // No 'base' line, no 'build' object. 
   // Let Vite handle the paths automatically.
 });
