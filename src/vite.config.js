0830b797c5601673ea10396bee39eb5375228015
pascal masila <118198832+Mugiwara1uffy@users.noreply.github.com>
2026-03-18 09:35:21 +0300

    Hard reset of paths and fixed App typos

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", 
+  // No 'base' line, no 'build' object. 
+  // Let Vite handle the paths automatically.
 });
